5a65101fa04d5e6f4e32221b2474adf0c189ec71
Jonas Pessini <jonaspessinidigital@gmail.com>
2026-05-09 18:46:10 -0300

    teste

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gastos-mensal-v4.9.9';
+const CACHE_NAME = 'gastos-mensal-v4.9.8';
 
 // Apenas assets verdadeiramente estáticos — CSS, fontes, imagens
 const ASSETS_TO_CACHE = [
