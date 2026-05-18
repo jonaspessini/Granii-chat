c4e09c40745b500c7d1e0b8848a85ea0b1760a6e
Jonas Pessini <jonaspessinidigital@gmail.com>
2026-05-18 20:02:44 -0300

    edit desp cartao

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gastos-mensal-v4.0.9';
+const CACHE_NAME = 'gastos-mensal-v4.0.8';
 const FONTS_CACHE = 'gastos-fontes-v1';
 
 // Apenas assets verdadeiramente estáticos — CSS, fontes, imagens
